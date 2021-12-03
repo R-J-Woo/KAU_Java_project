2189faa23a5061137db0e76d049840d8a6f1f14f
R-J-Woo <68835451+R-J-Woo@users.noreply.github.com>
2021-12-03 21:49:11 +0900

    안드로이드 프로젝트입니다.

---
+++ src/main/java/com/example/bookinfo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.bookinfo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View v) {
+        Toast.makeText(getApplicationContext(), "도서를 찾을 수 없습니다.", Toast.LENGTH_LONG).show();
+    }
+}
